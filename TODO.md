63b83ea56510aa7dffea8549de8bfd9c1486d4d7
Tom Tatchell <tom@buzzbox.eu>
2017-01-26 17:13:28 +0000

    Updated TODO Completed logic for Refraction, Diffuse 01, Specular 01 and 02, Luminosity and bump. Tested and working in C4D

--- TODO.md
+++ TODO.md
@@ -9,6 +9,15 @@
  - [x] Check for Texture Map  
  - [x] Check for RGB vectors  
 
-- [x] Fix VRay to Arnold Anisotropy rotation conversion  
-- [ ] Create Get and Set for specular channel 2  
-- [ ] Create Get and Set Bump (Logic needed for adding bump **After** the alSurface node)
+- [x] Fix VRay to Arnold Anisotropy rotation conversion
+- [x] Create Get and Set for specular channel 2
+- [x] Create Get and Set Bump
+- [ ] Method for adding SSS
+- [ ] Method for adding 2nd diffuse layer
+- [ ] Method for adding specular layers 3, 4 and 5
+- [ ] Method for adding Material Weight
+- [ ] Method for adding Material Matte  
+
+- [ ] Get Script running for selected material only **OR** all VRay materials
+- [ ] Have option to replace existing VRay material assignments or not
+- [ ] Have option to create tags on objects if material has transparency of some sort (Could just be an option button, if the layer contains any variation on 100% opacity)
--- TexConvert_v0.1.py
+++ TexConvert_v0.1.py
@@ -24,6 +24,7 @@ ARNOLD_DISPLACEMENT_PORT_ID = 537905100
 # from api/util/NodeIds.h
 C4DAIN_STANDARD = 1760354454
 C4DAIN_NOISE = 268710787
+C4DAIN_BUMP2D = 184848913
 
 # from res/description/ainode_standard.h
 C4DAIP_STANDARD_KD_COLOR = 338192719
@@ -34,6 +35,12 @@ C4DAIP_STANDARD_KS_COLOR = 1853957632
 C4DAIP_NOISE_OCTAVES = 35478650
 C4DAIP_NOISE_DISTORTION = 1840107712
 
+# from res/description/ainode_bump2d.h
+C4DAIP_BUMP2D_BUMP_MAP = 1722542866
+C4DAIP_BUMP2D_BUMP_HEIGHT = 149592681
+C4DAIP_BUMP2D_SHADER = 1367502316
+
+
 # alShader nodes
 ALSURFACE_NODE = 815861509
 
@@ -41,13 +48,31 @@ ALSURFACE_NODE = 815861509
 C4DAIP_ALSURFACE_DIFFUSESTRENGTH = 1212163586
 C4DAIP_ALSURFACE_DIFFUSECOLOR = 1076269102
 C4DAIP_ALSURFACE_DIFFUSEROUGHNESS = 1994027635
+
 C4DAIP_ALSURFACE_OPACITY = 234584834
+
 C4DAIP_ALSURFACE_SPECULAR1STRENGTH = 959965816
 C4DAIP_ALSURFACE_SPECULAR1COLOR = 1968924664
 C4DAIP_ALSURFACE_SPECULAR1IOR = 1098234115
 C4DAIP_ALSURFACE_SPECULAR1ROUGHNESS = 2033531479
 C4DAIP_ALSURFACE_SPECULAR1ANISOTROPY = 2145841423
 C4DAIP_ALSURFACE_SPECULAR1ROTATION = 1577603657
+
+C4DAIP_ALSURFACE_SPECULAR2STRENGTH = 994346633
+C4DAIP_ALSURFACE_SPECULAR2COLOR = 2008060057
+C4DAIP_ALSURFACE_SPECULAR2IOR = 1098270052
+C4DAIP_ALSURFACE_SPECULAR2ROUGHNESS = 2101332856
+C4DAIP_ALSURFACE_SPECULAR2ANISOTROPY = 91604018
+C4DAIP_ALSURFACE_SPECULAR2ROTATION = 763051190
+
+C4DAIP_ALSURFACE_TRANSMISSIONSTRENGTH = 1356953022
+C4DAIP_ALSURFACE_TRANSMISSIONCOLOR = 84572430
+C4DAIP_ALSURFACE_TRANSMISSIONROUGHNESS = 1817855569
+C4DAIP_ALSURFACE_TRANSMISSIONIOR = 769142403
+C4DAIP_ALSURFACE_TRANSMISSIONLINKTOSPECULAR1 = 1303558196
+
+C4DAIP_ALSURFACE_EMISSIONSTRENGTH = 533840799
+C4DAIP_ALSURFACE_EMISSIONCOLOR = 1546843503
 
 # from res/description/ainode_image.h
 C4DAIP_IMAGE_FILENAME = 1737748425
@@ -215,7 +240,6 @@ def GetDiffuse_01():
         Diffuse_01['DLT_TexMapPath'] = mat[c4d.VRAYMATERIAL_COLOR1_TRANSPSHADER][c4d.BITMAPSHADER_FILENAME]
     if Diffuse_01['DO_TextureMap'] != None:
         Diffuse_01['DO_TexMapPath'] = mat[c4d.VRAYMATERIAL_COLOR1_ROUGHNESSTEX][c4d.BITMAPSHADER_FILENAME]
-
     return Diffuse_01
 
 def SetDiffuse_01(mat, alSurface, VD01, Layers):
@@ -267,13 +291,12 @@ def GetSpecular_01():
     'SLP_TextureMap' : mat[c4d.VRAYMATERIAL_SPECULAR1_REFLECTIONGLOSSSHADER], # c4d.BaseShader object
     'SLP_TexMapPath' : None,
     'SLA_Anisotropy' : mat[c4d.VRAYMATERIAL_SPECULAR1_ANISOTROPY], # float
-    'SLA_AniRot' : mat[c4d.VRAYMATERIAL_SPECULAR1_ANISOTROPYROT], # fload
+    'SLA_AniRot' : mat[c4d.VRAYMATERIAL_SPECULAR1_ANISOTROPYROT], # float
     }
     if Specular_01['SC_TextureMap'] != None:
         Specular_01['SC_TexMapPath'] = mat[c4d.VRAYMATERIAL_SPECULAR1_SHADER][c4d.BITMAPSHADER_FILENAME]
     if Specular_01['SLP_TextureMap'] != None:
         Specular_01['SLP_TexMapPath'] = mat[c4d.VRAYMATERIAL_SPECULAR1_REFLECTIONGLOSSSHADER][c4d.BITMAPSHADER_FILENAME]
-
     return Specular_01
 
 
@@ -289,7 +312,7 @@ def SetSpecular_01(mat, alSurface, VS01, Layers):
         VtoA_ani = ((VS01['SLA_Anisotropy'] + 1) / 2)
         alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR1ANISOTROPY, VtoA_ani)
 
-        VtoA_aniRot = rangeMap(mat[c4d.VRAYMATERIAL_SPECULAR1_ANISOTROPYROT], 6.28318530718, 0, 1, 0)
+        VtoA_aniRot = rangeMap(VS01['SLA_AniRot'], 6.28318530718, 0, 1, 0)
         alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR1ROTATION, VtoA_aniRot)
 
         # create bitmaps / images
@@ -313,6 +336,181 @@ def SetSpecular_01(mat, alSurface, VS01, Layers):
     else:
          alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR1STRENGTH, 0.0)
 
+
+def GetSpecular_02():
+    Specular_02 = {
+    'SC_Colour' : mat[c4d.VRAYMATERIAL_SPECULAR2_COLOR], # c4d Vector
+    'SC_Brightness' : mat[c4d.VRAYMATERIAL_SPECULAR2_COLOR_MULT], # float
+    'SC_TextureMap' : mat[c4d.VRAYMATERIAL_SPECULAR2_SHADER], # c4d.BaseShader object
+    'SC_TexMapPath' : None,
+    'SLT_IOR' : mat[c4d.VRAYMATERIAL_SPECULAR2_FRESNELIOR1], # float
+    'SLP_RefGloss' : mat[c4d.VRAYMATERIAL_SPECULAR2_REFLECTIONGLOSS], # float
+    'SLP_TextureMap' : mat[c4d.VRAYMATERIAL_SPECULAR2_REFLECTIONGLOSSSHADER], # c4d.BaseShader object
+    'SLP_TexMapPath' : None,
+    'SLA_Anisotropy' : mat[c4d.VRAYMATERIAL_SPECULAR2_ANISOTROPY], # float
+    'SLA_AniRot' : mat[c4d.VRAYMATERIAL_SPECULAR2_ANISOTROPYROT], # float
+    }
+    if Specular_02['SC_TextureMap'] != None:
+        Specular_02['SC_TexMapPath'] = mat[c4d.VRAYMATERIAL_SPECULAR2_SHADER][c4d.BITMAPSHADER_FILENAME]
+    if Specular_02['SLP_TextureMap'] != None:
+        Specular_02['SLP_TexMapPath'] = mat[c4d.VRAYMATERIAL_SPECULAR2_REFLECTIONGLOSSSHADER][c4d.BITMAPSHADER_FILENAME]
+    return Specular_02
+
+
+def SetSpecular_02(mat, alSurface, VS02, Layers):
+    if 'Spec_02' in Layers:
+        alSurface.GetOpContainerInstance().SetVector(C4DAIP_ALSURFACE_SPECULAR2COLOR, VS02['SC_Colour'])
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR2STRENGTH, VS02['SC_Brightness'])
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR2IOR, VS02['SLT_IOR'])
+
+        VtoA_roughness = 1.0 - VS02['SLP_RefGloss']  # convert vray 0-1 value to c4dtoa 1-0 value
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR2ROUGHNESS, VtoA_roughness)
+
+        VtoA_ani = ((VS02['SLA_Anisotropy'] + 1) / 2)
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR2ANISOTROPY, VtoA_ani)
+
+        VtoA_aniRot = rangeMap(VS02['SLA_AniRot'], 6.28318530718, 0, 1, 0)
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR2ROTATION, VtoA_aniRot)
+
+        # create bitmaps / images
+        if VS02['SC_TextureMap'] and VS02['SC_TextureMap'].GetType() == c4d.Xbitmap:
+            SCBitmap = CreateC4DShader(mat, c4d.Xbitmap, -250, 150)
+            if SCBitmap is None:
+                raise Exception("Failed to create Bitmap shader")
+            SCBitmap.SetName("Specular Colour Bitmap")
+            SCBitmapShader = SCBitmap.GetFirstShader()
+            SCBitmapShader.GetDataInstance().SetFilename(c4d.BITMAPSHADER_FILENAME, VS02['SC_TexMapPath'])
+            AddConnection(mat, SCBitmap, alSurface, C4DAIP_ALSURFACE_SPECULAR2COLOR)
+
+        if VS02['SLP_TextureMap'] and VS02['SLP_TextureMap'].GetType() == c4d.Xbitmap:
+            SLPBitmap = CreateC4DShader(mat, c4d.Xbitmap, -250, 100)
+            if SLPBitmap is None:
+                raise Exception("Failed to create Bitmap shader")
+            SLPBitmap.SetName("Specular Roughness Bitmap")
+            SLPBitmapShader = SLPBitmap.GetFirstShader()
+            SLPBitmapShader.GetDataInstance().SetFilename(c4d.BITMAPSHADER_FILENAME, VS02['SLP_TexMapPath'])
+            AddConnection(mat, SLPBitmap, alSurface, C4DAIP_ALSURFACE_SPECULAR2ROUGHNESS)
+    else:
+         alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_SPECULAR2STRENGTH, 0.0)
+
+
+def GetTransmission():
+    transmission = {
+    'RC_Colour' : mat[c4d.VRAYMATERIAL_TRANSPARENCY_COLOR], # c4d Vector
+    'RC_Brightness' : mat[c4d.VRAYMATERIAL_TRANSPARENCY_COLOR_MULT], # float
+    'RC_TextureMap' : mat[c4d.VRAYMATERIAL_TRANSPARENCY_SHADER], # c4d.BaseShader object
+    'RC_TexMapPath' : None,
+    'RLP_IOR' : mat[c4d.VRAYMATERIAL_TRANSPARENCY_IOR], # float
+    'RLP_Gloss' : mat[c4d.VRAYMATERIAL_TRANSPARENCY_GLOSSINESS], # float
+    'RLP_TextureMap' : mat[c4d.VRAYMATERIAL_TRANSPARENCY_GLOSSINESSSHADER], # c4d.BaseShader object
+    'RLP_TexMapPath' : None,
+    }
+    if transmission['RC_TextureMap'] != None:
+        transmission['RC_TexMapPath'] = mat[c4d.VRAYMATERIAL_TRANSPARENCY_SHADER][c4d.BITMAPSHADER_FILENAME]
+    if transmission['RLP_TextureMap'] != None:
+        transmission['RLP_TexMapPath'] = mat[c4d.VRAYMATERIAL_TRANSPARENCY_GLOSSINESSSHADER][c4d.BITMAPSHADER_FILENAME]
+    return transmission
+
+
+def SetTransmission(mat, alSurface, VT, Layers):
+    if 'Refraction' in Layers:
+        alSurface.GetOpContainerInstance().SetVector(C4DAIP_ALSURFACE_TRANSMISSIONCOLOR, VT['RC_Colour'])
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_TRANSMISSIONSTRENGTH, VT['RC_Brightness'])
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_TRANSMISSIONIOR, VT['RLP_IOR'])
+
+        VtoA_roughness = 1.0 - VT['RLP_Gloss']  # convert vray 0-1 value to c4dtoa 1-0 value
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_TRANSMISSIONROUGHNESS, VtoA_roughness)
+
+        # create bitmaps / images
+        if VT['RC_TextureMap'] and VT['RC_TextureMap'].GetType() == c4d.Xbitmap:
+            RCBitmap = CreateC4DShader(mat, c4d.Xbitmap, -250, 150)
+            if RCBitmap is None:
+                raise Exception("Failed to create Bitmap shader")
+            RCBitmap.SetName("Transmission Colour Bitmap")
+            RCBitmapShader = RCBitmap.GetFirstShader()
+            RCBitmapShader.GetDataInstance().SetFilename(c4d.BITMAPSHADER_FILENAME, VT['RC_TexMapPath'])
+            AddConnection(mat, RCBitmap, alSurface, C4DAIP_ALSURFACE_TRANSMISSIONCOLOR)
+
+        if VT['RLP_TextureMap'] and VT['RLP_TextureMap'].GetType() == c4d.Xbitmap:
+            RLPBitmap = CreateC4DShader(mat, c4d.Xbitmap, -250, 100)
+            if RLPBitmap is None:
+                raise Exception("Failed to create Bitmap shader")
+            RLPBitmap.SetName("Transmission Roughness Bitmap")
+            RLPBitmapShader = SLPBitmap.GetFirstShader()
+            RLPBitmapShader.GetDataInstance().SetFilename(c4d.BITMAPSHADER_FILENAME, VT['RLP_TexMapPath'])
+            AddConnection(mat, RLPBitmap, alSurface, C4DAIP_ALSURFACE_TRANSMISSIONROUGHNESS)
+
+        if 'Spec_01' in Layers:
+            alSurface.GetOpContainerInstance().SetInt32(C4DAIP_ALSURFACE_TRANSMISSIONLINKTOSPECULAR1, 1)
+        elif 'Spec_01' not in Layers:
+            alSurface.GetOpContainerInstance().SetInt32(C4DAIP_ALSURFACE_TRANSMISSIONLINKTOSPECULAR1, 0)
+    else:
+         alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_TRANSMISSIONSTRENGTH, 0.0)
+
+
+def GetEmission():
+        emission = {
+        'LC_Colour' : mat[c4d.VRAYMATERIAL_LUMINANCE_COLOR], # c4d Vector
+        'LC_Amount' : mat[c4d.VRAYMATERIAL_LUMINANCE_COLOR_MULT], # float
+        'LC_TextureMap' : mat[c4d.VRAYMATERIAL_LUMINANCE_SHADER], # c4d.BaseShader object
+        'LC_TexMapPath' : None,
+        }
+        if emission['LC_TextureMap'] != None:
+            emission['LC_TexMapPath'] = mat[c4d.VRAYMATERIAL_LUMINANCE_SHADER][c4d.BITMAPSHADER_FILENAME]
+        return emission
+
+
+def SetEmission(mat, alSurface, VE, Layers):
+    if 'Luminosity' in Layers:
+        alSurface.GetOpContainerInstance().SetVector(C4DAIP_ALSURFACE_EMISSIONCOLOR, VE['LC_Colour'])
+        alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_EMISSIONSTRENGTH, VE['LC_Amount'])
+
+        # create bitmaps / images
+        if VE['LC_TextureMap'] and VE['LC_TextureMap'].GetType() == c4d.Xbitmap:
+            LCBitmap = CreateC4DShader(mat, c4d.Xbitmap, -250, 150)
+            if LCBitmap is None:
+                raise Exception("Failed to create Bitmap shader")
+            LCBitmap.SetName("Transmission Colour Bitmap")
+            LCBitmapShader = LCBitmap.GetFirstShader()
+            LCBitmapShader.GetDataInstance().SetFilename(c4d.BITMAPSHADER_FILENAME, VE['LC_TexMapPath'])
+            AddConnection(mat, LCBitmap, alSurface, C4DAIP_ALSURFACE_EMISSIONCOLOR)
+    else:
+         alSurface.GetOpContainerInstance().SetFloat(C4DAIP_ALSURFACE_EMISSIONSTRENGTH, 0.0)
+
+
+def GetBump():
+    bump = {
+    'BTextureMap' : mat[c4d.VRAYMATERIAL_BUMP_SHADER], # c4d.BaseShader object
+    'BTexMapPath' : None,
+    'BAmount' : mat[c4d.VRAYMATERIAL_BUMP_BUMPTEXMULT] # float
+    }
+    if bump['BTextureMap'] != None:
+        bump['BTexMapPath'] = mat[c4d.VRAYMATERIAL_BUMP_SHADER][c4d.BITMAPSHADER_FILENAME]
+    return bump
+
+
+def SetBump(mat, alSurface, VB, Layers):
+    if 'Bump' in Layers:
+        VBump = CreateArnoldShader(mat, C4DAIN_BUMP2D, 400, 100)
+        if VBump is None:
+            raise Exception("Failed to create bump2d node")
+        VBump.SetName("VtoA_Bump2D")
+        VBump.GetOpContainerInstance().SetFloat(C4DAIP_BUMP2D_BUMP_HEIGHT, VB['BAmount'])
+
+        # create bitmaps / images
+        if VB['BTextureMap'] and VB['BTextureMap'].GetType() == c4d.Xbitmap:
+            VBBitmap = CreateC4DShader(mat, c4d.Xbitmap, 0, 0)
+            if VBBitmap is None:
+                raise Exception("Failed to create bitmap shader")
+            VBBitmap.SetName("VtaA Bump Bitmap shader")
+            VBBitmapShader = VBBitmap.GetFirstShader()
+            VBBitmapShader.GetDataInstance().SetFilename(c4d.BITMAPSHADER_FILENAME, VB['BTexMapPath'])
+            AddConnection(mat, VBBitmap, VBump, C4DAIP_BUMP2D_BUMP_MAP)
+
+        AddConnection(mat, alSurface, VBump, C4DAIP_BUMP2D_SHADER)
+        SetRootShader(mat, VBump, ARNOLD_BEAUTY_PORT_ID)
+
+
 def main():
     doc = c4d.documents.GetActiveDocument()
     activemat = doc.GetActiveMaterial()
@@ -320,12 +518,16 @@ def main():
 
     VD01 = GetDiffuse_01()
     VS01 = GetSpecular_01()
+    VS02 = GetSpecular_02()
+    VT = GetTransmission()
+    VE = GetEmission()
+    VB = GetBump()
 
     # create material
     mat = c4d.BaseMaterial(ARNOLD_SHADER_NETWORK)
     if mat is None:
         raise Exception("Failed to create material")
-    mat.SetName("mat01")
+    mat.SetName("VtoA01")
     doc.InsertMaterial(mat)
 
     # create shaders
@@ -335,13 +537,17 @@ def main():
 
 
     # set shader parameters
-    alSurface.SetName("VRay to A")
-
+    alSurface.SetName("VtoAlSurface")
     SetDiffuse_01(mat, alSurface, VD01, Layers)
     SetSpecular_01(mat, alSurface, VS01, Layers)
+    SetSpecular_02(mat, alSurface, VS02, Layers)
+    SetTransmission(mat, alSurface, VT, Layers)
+    SetEmission(mat, alSurface, VE, Layers)
+    SetBump(mat, alSurface, VB, Layers)
 
-    # connect shaders
-    SetRootShader(mat, alSurface, ARNOLD_BEAUTY_PORT_ID)
+    # connect root shader
+    if 'Bump' not in Layers:
+        SettRootShader(mat, alSurface, ARNOLD_BEAUTY_PORT_ID)
 
     # redraw
     c4d.EventAdd(c4d.EVENT_FORCEREDRAW)
